451be9018375af8383622867989ddfdccb4f0073
as <>
2021-06-23 08:39:41 +0900

    asset import

---
+++ Assets/MyScript/AssignTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignTexture : MonoBehaviour
+{
+    public ComputeShader shader;
+    public int texResolution = 256;
+
+    Renderer rend;
+    RenderTexture outputTexture;
+    int kernelHandle;
+    
+    void Start()
+    {
+        outputTexture = new RenderTexture(texResolution, texResolution, 0);
+        outputTexture
+    }
+
+    void Update()
+    {
+        
+    }
+}
